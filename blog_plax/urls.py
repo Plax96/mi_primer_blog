a3d87ab35b1a47be4bfa4b9f3dd0176c18dc745e
Plax96 <placido.castellanos@gmail.com>
2017-09-25 23:03:51 -0600

    seleccionar una pantalla

--- blog_plax/urls.py
+++ blog_plax/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>\d+)/$', views.publicacion_detalle, name='publicacion_detalle'),
 ]
--- blog_plax/views.py
+++ blog_plax/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 def post_list(request):
     publicaciones= Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blog/post_list.html', {'publicaciones':publicaciones})
+
+def publicacion_detalle(request, pk):
+    publicacion = get_object_or_404(Publicacion, id=pk)
+    return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
