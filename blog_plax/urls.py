3f4cd015bbd74e8f6fb1b99103f63805da91cd33
Plax96 <placido.castellanos@gmail.com>
2017-09-26 09:47:56 -0600

    publicar y editar

--- blog_plax/urls.py
+++ blog_plax/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>\d+)/$', views.publicacion_detalle, name='publicacion_detalle'),
+    url(r'^post/nueva/$', views.publicacion_nueva, name='publicacion_nueva'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.publicacion_editar, name='publicacion_editar'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
 ]
--- blog_plax/views.py
+++ blog_plax/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.utils import timezone
 from .models import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .forms import PublicacionForm
 
 def post_list(request):
     publicaciones= Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
@@ -8,3 +12,38 @@ def post_list(request):
 def publicacion_detalle(request, pk):
     publicacion = get_object_or_404(Publicacion, id=pk)
     return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
+
+def publicacion_nueva(request):
+    if request.method == "POST":
+        form = PublicacionForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('publicacion_editar', pk=post.pk)
+    else:
+        form = PublicacionForm()
+    return render(request, 'blog/publicacion_editar.html', {'form': form})
+
+def publicacion_editar(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PublicacionForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('publicacion_editar', pk=post.pk)
+    else:
+        form = PublicacionForm(instance=post)
+    return render(request, 'blog/publicacion_editar.html', {'form': form})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    post.publish()
+    return redirect('publicacion_detalle', pk=pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
